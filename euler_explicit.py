79da167b78b555627b8a9a9032f9572851510c52
Yassine AIT SAID <yaitsaid@enseirb-matmeca.fr>
2024-12-02 11:34:51 +0100

    ajout de B dans euler_explicit

--- euler_explicit.py
+++ euler_explicit.py
@@ -30,7 +30,7 @@ t_start = 0.0
 t_end = 10.0
 dt = 0.01  
 
-# Appel de la fonction
+# test
 time, eta_e, eta_A, eta_B = euler_explicit_1(sigma_0, V, eta_e0, eta_A0, eta_B0, t_start, t_end, dt)
 
 # Affichage des résultats
@@ -40,25 +40,22 @@ plt.plot(time, eta_A, label=r'$\eta_A$', color='red')
 plt.plot(time, eta_B, label=r'$\eta_B$', color='green')
 plt.xlabel('Temps (t)')
 plt.ylabel(r'$\eta$')
-plt.title('Évolution de $\eta_E$ et $\eta_A$ (Euler explicite)')
+plt.title('Évolution de $\eta_e$ et $\eta_A$ (Euler explicite)')
 plt.legend()
 plt.grid()
 plt.show()
 
 
 def euler_explicit_2(sigma_0, V, eta_e0, eta_A0, eta_B0, t_start, t_end, dt):
-    # Initialisation du temps et des variables
     time = np.arange(t_start, t_end, dt)
     eta_e = np.zeros_like(time)
     eta_A = np.zeros_like(time)
     eta_B = np.zeros_like(time)
     
-    # Conditions initiales
     eta_e[0] = eta_e0
     eta_A[0] = eta_A0
     eta_B[0] = eta_B0
 
-    # Boucle temporelle
     for i in range(1, len(time)):
         common_term = -sigma_0 / (V * V) * eta_A[i - 1] * eta_e[i - 1] * eta_B[i - 1]
         d_eta_e = common_term
@@ -81,7 +78,6 @@ t_start = 0.0
 t_end = 10.0
 dt = 0.01  
 
-# Appel de la fonction
 time, eta_e, eta_A, eta_B = euler_explicit_2(sigma_0, V, eta_e0, eta_A0, eta_B0, t_start, t_end, dt)
 
 # Affichage des résultats
@@ -91,7 +87,7 @@ plt.plot(time, eta_A, label=r'$\eta_A$', color='red')
 plt.plot(time, eta_B, label=r'$\eta_B$', color='green')
 plt.xlabel('Temps (t)')
 plt.ylabel(r'$\eta$')
-plt.title('Évolution de $\eta_E$, $\eta_A$, et $\eta_B$ (Euler explicite)')
+plt.title('Évolution de $\eta_e$, $\eta_A$, et $\eta_B$ (Euler explicite)')
 plt.legend()
 plt.grid()
 plt.show()
