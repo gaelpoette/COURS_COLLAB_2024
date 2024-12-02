a8f00438e3982f44bdf0894e5b18625abde22926
Yassine AIT SAID <yaitsaid@enseirb-matmeca.fr>
2024-12-02 11:29:53 +0100

    ajout de B dans euler_explicit

--- euler_explicit.py
+++ euler_explicit.py
@@ -1,37 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def euler_explicit_1(sigma_0, V, eta_e0, eta_A0, t_start, t_end, dt):
+def euler_explicit_1(sigma_0, V, eta_e0, eta_A0, eta_B0, t_start, t_end, dt):
     time = np.arange(t_start, t_end, dt)
     eta_e = np.zeros_like(time)
     eta_A = np.zeros_like(time)
+    eta_B = np.zeros_like(time)
     eta_e[0] = eta_e0
     eta_A[0] = eta_A0
+    eta_B[0] = eta_B0
     
     for i in range(1, len(time)):
         d_eta_e = -sigma_0 / V * eta_A[i - 1] * eta_e[i - 1]
         d_eta_A = -sigma_0 / V * eta_A[i - 1] * eta_e[i - 1]
+        d_eta_B = sigma_0 / V * eta_A[i - 1] * eta_e[i - 1]
         eta_e[i] = eta_e[i - 1] + dt * d_eta_e
         eta_A[i] = eta_A[i - 1] + dt * d_eta_A
+        eta_B[i] = eta_B[i - 1] + dt * d_eta_B
 
-    return time, eta_e, eta_A
+    return time, eta_e, eta_A, eta_B
 
 # test
 sigma_0 = 1.0  
 V = 1.0       
 eta_e0 = 1.0  
-eta_A0 = 1.0   
+eta_A0 = 1.0
+eta_B0 = 1.0   
 t_start = 0.0
 t_end = 10.0
 dt = 0.01  
 
 # Appel de la fonction
-time, eta_e, eta_A = euler_explicit_1(sigma_0, V, eta_e0, eta_A0, t_start, t_end, dt)
+time, eta_e, eta_A, eta_B = euler_explicit_1(sigma_0, V, eta_e0, eta_A0, eta_B0, t_start, t_end, dt)
 
 # Affichage des résultats
 plt.figure(figsize=(10, 6))
 plt.plot(time, eta_e, label=r'$\eta_e$', color='blue')
 plt.plot(time, eta_A, label=r'$\eta_A$', color='red')
+plt.plot(time, eta_B, label=r'$\eta_B$', color='green')
 plt.xlabel('Temps (t)')
 plt.ylabel(r'$\eta$')
 plt.title('Évolution de $\eta_E$ et $\eta_A$ (Euler explicite)')
